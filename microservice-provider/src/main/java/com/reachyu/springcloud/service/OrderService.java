069fd48c7e8d58d7f6574f77c828608886b5172e
reachyu <y-ey@163.com>
2020-07-28 14:58:54 +0800

    去掉代码静态检查问题

--- microservice-provider/src/main/java/com/reachyu/springcloud/service/OrderService.java
+++ microservice-provider/src/main/java/com/reachyu/springcloud/service/OrderService.java
@@ -9,9 +9,9 @@ import java.util.List;
  */
 public interface OrderService {
 
-    public boolean addOrder(Order order);
+    boolean addOrder(Order order);
 
-    public Order getOrder(String id);
+    Order getOrder(String id);
 
-    public List<Order> listOrder();
+    List<Order> listOrder();
 }
